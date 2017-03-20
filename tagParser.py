e15bd4b384bc4df212bfb137dfbc8ffac4ba697f
khali119 <khali119@umn.edu>
2017-03-20 12:49:28 -0500

    added tag parser & updated projList

---
+++ tagParser.py
@@ -0,0 +1,39 @@
+projMap = {}
+loc = 'projList'
+key = ''
+
+with open(loc) as File:
+	for line in File:
+
+		if '</' in line or line == '\n':
+			pass
+
+		elif '<' in line:
+			key = line.strip()[1:-1]
+			projMap[key] = []
+
+		else:
+			projMap[key].append(line.strip())
+
+def genHtml(zeProjMap, filename):
+	htmlFile = open(filename, 'w')
+	htmlFile.write('<html>\n<body>\n')
+
+	for key, value in zeProjMap.iteritems():
+		htmlFile.write('\t<h2>%s</h2>\n\t<ul>\n' % key)
+		for item in value:
+			htmlFile.write('\t\t<li><a href="%s" target="_blank">%s</a></li>\n' % (item, item))
+		htmlFile.write('</ul>\n')
+	htmlFile.close()
+
+def printMap(zeProjMap):
+	for key, value in zeProjMap.iteritems():
+		print key
+		for item in value:
+			print '   ', item
+		print ''
+
+printMap(projMap)
+genHtml(projMap, 'map.html')
+
+
